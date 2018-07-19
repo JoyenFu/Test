5bf0f46880c7718f6d60f1c27f978e1fcef47b9f
fuwenjun <598141154@qq.com>
2018-07-19 13:24:35 +0800

    v2 first commit

--- testsub1/src/main/java/testsub1/App.java
+++ testsub1/src/main/java/testsub1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World OPP !" );
+        System.out.println( "Hello World v2 版本分支 !" );
     }
 }
