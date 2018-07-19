52fd7da8daef7845ddb67fb5d0a9c02fdddf2ebe
fuwenjun <598141154@qq.com>
2018-07-19 16:45:53 +0800

    B

--- testsub1/src/main/java/testsub1/App.java
+++ testsub1/src/main/java/testsub1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World 分支v3!A" );
+        System.out.println( "Hello World 分支v3!B" );
     }
 }
