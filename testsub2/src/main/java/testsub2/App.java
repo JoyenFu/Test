c89fd544cf674b5f0372cd6563148e2a167860f0
fuwenjun <598141154@qq.com>
2018-07-20 14:44:41 +0800

    local v2

--- testsub2/src/main/java/testsub2/App.java
+++ testsub2/src/main/java/testsub2/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World v2 特性 online!" );
+        System.out.println( "Hello World v2 特性 online v2!" );
     }
 }
