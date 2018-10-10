11207663d604cc0e432080a7d690917ee667aa3f
fuwenjun <598141154@qq.com>
2018-10-10 20:44:03 +0800

    分词
    by fuwenjn

--- testsub1/src/main/java/testsub1/WordFenCi.java
+++ testsub1/src/main/java/testsub1/WordFenCi.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apdplat.word.WordSegmenter;
 import org.apdplat.word.recognition.PersonName;
+import org.apdplat.word.segmentation.SegmentationAlgorithm;
 import org.apdplat.word.segmentation.Word;
 import org.apdplat.word.segmentation.WordRefiner;
 
@@ -12,11 +13,12 @@ public class WordFenCi {
     public static void main(String[] args) {
         // 严格18233333333上海上海市浦东新区管委会
         // 严格18233333333上海上海市浦东新区国际中心
-        List<Word> words = WordSegmenter.segWithStopWords("付文军18233333333陕西省西安市雁塔区大寨路99号宏府麒麟山9号楼2单元");
+        //付文军18233333333陕西省西安市雁塔区大寨路99号宏府麒麟山9号楼2单元
+        List<Word> words = WordSegmenter.segWithStopWords("严格18233333333陕西西安雁塔国际中心",SegmentationAlgorithm.MinimalWordCount);
         //words = WordRefiner.refine(words);
-        //List<Word> nameWords = PersonName.recognize(words);
+        List<Word> nameWords = PersonName.recognize(words);
         System.out.println(words);
-        //System.out.println(nameWords);
+        System.out.println(nameWords);
     }
 
 }
