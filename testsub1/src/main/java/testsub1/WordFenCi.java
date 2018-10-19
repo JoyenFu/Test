b31da9bb1881ca3276bb745c007567cf93221c67
fuwenjun <598141154@qq.com>
2018-10-19 21:36:31 +0800

    haha
    by fuwenjun

--- testsub1/src/main/java/testsub1/WordFenCi.java
+++ testsub1/src/main/java/testsub1/WordFenCi.java
@@ -3,13 +3,15 @@ package testsub1;
 import java.util.List;
 
 import org.apdplat.word.WordSegmenter;
+import org.apdplat.word.segmentation.SegmentationAlgorithm;
 import org.apdplat.word.segmentation.Word;
 
 public class WordFenCi {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        List<Word> words = WordSegmenter.segWithStopWords("上海上海市浦东新区国际中心(A3-2)");
+        List<Word> words = WordSegmenter.segWithStopWords("地址上海上海市浦东新区国际中心(A3-2)",SegmentationAlgorithm.MinimalWordCount);
+        StopWord.filterStopWords(words);
         System.out.println(words);  
     }
 
