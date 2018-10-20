f3caf1575e49052c4ac88dd9b20b788680909272
fuwenjun <598141154@qq.com>
2018-10-20 09:08:32 +0800

    hah
    by fuwenjun

--- testsub1/src/main/java/testsub1/WordFenCi.java
+++ testsub1/src/main/java/testsub1/WordFenCi.java
@@ -14,11 +14,13 @@ public class WordFenCi {
         // 严格18233333333上海上海市浦东新区管委会
         // 严格18233333333上海上海市浦东新区国际中心
         //付文军18233333333陕西省西安市雁塔区大寨路99号宏府麒麟山9号楼2单元
-        List<Word> words = WordSegmenter.segWithStopWords("严格18233333333陕西西安雁塔国际中心",SegmentationAlgorithm.MinimalWordCount);
+        List<Word> words = WordSegmenter.segWithStopWords("收货人：段茵手机号码15249047756所在地区：上海上海市青浦区华新镇详细地址：华隆路1790号",SegmentationAlgorithm.MaximumMatching);
         //words = WordRefiner.refine(words);
-        List<Word> nameWords = PersonName.recognize(words);
+        //List<Word> nameWords = PersonName.recognize(words);
         System.out.println(words);
-        System.out.println(nameWords);
+        //System.out.println(nameWords);
+        
+        System.out.println("陕西省西安市".indexOf("西安市"));
     }
 
 }
