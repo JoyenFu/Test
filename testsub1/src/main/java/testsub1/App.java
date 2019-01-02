ac2014ef7fff4dc31874e46d68611410f23aadb3
fuwenjun <598141154@qq.com>
2019-01-02 16:25:09 +0800

    yy
    yy

--- testsub1/src/main/java/testsub1/App.java
+++ testsub1/src/main/java/testsub1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World 分支v3!BCHH" );
+        System.out.println( "Hello World 分支v3!BCHHVV" );
     }
 }
