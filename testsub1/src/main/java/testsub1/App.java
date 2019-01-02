57e5d8b9a81380aa80bf3e60ea4486c8dcc5ddf7
fuwenjun <598141154@qq.com>
2019-01-02 16:29:53 +0800

    Revert "测试push后回退"
    This reverts commit 222a118e22d4a1677090b0c45ce9bd0e3c704b30.

--- testsub1/src/main/java/testsub1/App.java
+++ testsub1/src/main/java/testsub1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World 分支v3!!" );
+        System.out.println( "Hello World 分支v3!BCHHVV" );
     }
 }
